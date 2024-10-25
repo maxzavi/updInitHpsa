94422f1d17d57ba3cd60ea94aafeacb26ade4bea
Max Zavaleta <mzavaletav@gmail.com>
2024-10-25 18:36:03 -0500

    fix: econ

--- src/repository/inputRepository.js
+++ src/repository/inputRepository.js
@@ -3,7 +3,7 @@ const { convert } = require('../config/convert')
 
 const getItemsInput = async ()=>{
     const filepath= process.env.FILE_PATH
-    const data = fs.readFileSync(filepath,'utf8')
+    const data = fs.readFileSync(filepath,process.env.ENCODE)
     const rows = data.split('\n')
 
     const tags = rows[0].replace("\r","").split(';')
@@ -13,7 +13,7 @@ const getItemsInput = async ()=>{
     const items = await csv({
         noheader:false,
         output: "csv",
-        delimiter: ";"
+        delimiter: process.env.DELIMITER
     })
     .fromString(data)
     .then((csvRow)=>{
